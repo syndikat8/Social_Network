c581bb10f5bb7e21044042df883a3c0f4504b0e4
Dima Matusik <k.syndikat@gmail.com>
2020-03-11 03:24:41 +0300

    add NavLink

--- src/App.js
+++ src/App.js
@@ -5,25 +5,25 @@ import Nav from "./components/Nav/Nav";
 import Profile from "./components/Profile/Profile";
 import Dialogs from "./components/Dialogs/Dialogs";
 import Music from "./components/Music/Music";
-import {BrowserRouter, Route} from "react-router-dom";
 import News from "./components/News/News";
 import Settings from "./components/Settings/Settings";
+import {BrowserRouter, Route} from "react-router-dom";
+
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="app-wrapper">
-      <Header/>
-      <Nav/>
-      <div className="app-wrapper-content">
-      <Route path="/dialogs" component={Dialogs}/>
-      <Route path="/profile" component={Profile}/>
-      <Route path="/music" component={Music}/>
-      <Route path="/news" component={News}/>
-      <Route path="/settings" component={Settings}/>
-
+      <div className="app-wrapper">
+        <Header/>
+        <Nav/>
+        <div className="app-wrapper-content">
+          <Route path="/profile" component={Profile}/>
+          <Route path="/dialogs" component={Dialogs}/>
+          <Route path="/music" component={Music}/>
+          <Route path="/news" component={News}/>
+          <Route path="/settings" component={Settings}/>
+        </div>
       </div>
-    </div>
     </BrowserRouter>
   );
 };
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,23 +1,24 @@
 import React from "react";
 import styles from './Nav.module.css';
+import {NavLink} from "react-router-dom";
 
 const Nav = () => {
   return (
     <nav className={styles.nav}>
       <div className={styles.item}>
-        <a href="/profile">Profile</a>
+        <NavLink to="/profile" activeClassName={styles.activeLink}>Profile</NavLink>
       </div>
       <div className={`${styles.item} ${styles.active}`}>
-        <a href="/dialogs">Messages</a>
+        <NavLink to="/dialogs" activeClassName={styles.activeLink}>Messages</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/news">News</a>
+        <NavLink to="/news" activeClassName={styles.activeLink}>News</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/music">Music</a>
+        <NavLink to="/music" activeClassName={styles.activeLink}>Music</NavLink>
       </div>
       <div className={styles.item}>
-        <a href="/settings">Settings</a>
+        <NavLink to="/settings" activeClassName={styles.activeLink}>Settings</NavLink>
       </div>
 
     </nav>
