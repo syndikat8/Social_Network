f64334a775ff918b766af601be5886c952319e32
Dima Matusik <k.syndikat@gmail.com>
2020-03-11 14:22:36 +0300

    add verstca dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -5,8 +5,31 @@ import styles from './Dialogs.module.css';
 const Dialogs = () => {
   return (
     <div className={styles.dialogs}>
-      Dialogs
-
+      <div className={styles.dialogs__items}>
+        <div className={styles.dialog + " " + styles.active}>
+          Dima
+        </div>
+        <div className={styles.dialog}>
+          Masha
+        </div>
+        <div className={styles.dialog}>
+          Sveta
+        </div>
+        <div className={styles.dialog}>
+          Sasha
+        </div>
+        <div className={styles.dialog}>
+          Victor
+        </div>
+        <div className={styles.dialog}>
+          Valera
+        </div>
+      </div >
+      <div className={styles.messages}>
+      <div className={styles.message}>Hi</div>
+      <div className={styles.message}>Hi is you</div>
+      <div className={styles.message}>You</div>
+      </div>
     </div>
   )
 };
