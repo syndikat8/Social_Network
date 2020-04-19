f37986ba0e6a2d184d6df657c2eee4912290c9ee
Dima Matusik <k.syndikat@gmail.com>
2020-04-19 03:09:14 +0300

    copy reducer

--- src/index.js
+++ src/index.js
@@ -7,24 +7,14 @@ import * as serviceWorker from './serviceWorker';
 import {BrowserRouter} from "react-router-dom";
 import {Provider} from "react-redux";
 
-
-
-let rerenderEntierTree = () => {
   ReactDOM.render(
     <BrowserRouter>
       <Provider store={store}>
       <App/>
       </Provider>
-    </BrowserRouter>
-    , document.getElementById('root'));
-};
+    </BrowserRouter>, document.getElementById('root'));
 
-rerenderEntierTree();
 
-store.subscribe(() => {
-  let state = store.getState();
-  rerenderEntierTree(state)
-});
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
