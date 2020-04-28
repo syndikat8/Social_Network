e45db79c8caa509b322761a7b70a93204e5b975e
Dima Matusik <k.syndikat@gmail.com>
2020-04-28 13:31:07 +0300

    add class component users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -4,46 +4,18 @@ import * as axios from "axios";
 import userPhoto from "../../assets/images/images.png"
 
 
-const Users = (props) => {
-
-  let getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
-        props.setUsers(response.data.items)
-      })
-      // props.setUsers([
-      //     {
-      //       id: 1,
-      //       photoUrl: "https://v1.popcornnews.ru/k2/news/970/upload/news/637056676986.jpg",
-      //       followed: false,
-      //       name: "Dima",
-      //       status: "I'm a boss",
-      //       location: {city: "Minsk", country: "Belarus"}
-      //     },
-      //     {
-      //       id: 2,
-      //       photoUrl: "https://v1.popcornnews.ru/k2/news/970/upload/news/637056676986.jpg",
-      //       followed: true,
-      //       name: "Vasya",
-      //       status: "Doter",
-      //       location: {city: "Ukraine", country: "Kiev"}
-      //     },
-      //     {
-      //       id: 3,
-      //       photoUrl: "https://v1.popcornnews.ru/k2/news/970/upload/news/637056676986.jpg",
-      //       followed: false,
-      //       fullName: "Bob",
-      //       status: "Cs go",
-      //       location: {city: "Moscow", country: "Russia"}
-      //     }
-      //   ])
-    }
+class Users extends React.Component {
+  constructor(props) {
+    super(props);
+    axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+      this.props.setUsers(response.data.items)
+    })
   }
-  return (
-    <div>
-      <button onClick={getUsers}>Get Users</button>
-      {
-        props.users.map(u => <div key={u.id}>
+
+  render() {
+    return (
+      <div>{
+        this.props.users.map(u => <div key={u.id}>
           <div className={styles.users}>
             <div className={styles.avaButton}>
               <div>
@@ -54,12 +26,11 @@ const Users = (props) => {
               <div>
                 {u.followed
                   ? <button onClick={() => {
-                    props.unfollow(u.id)
+                    this.props.unfollow(u.id)
                   }}>UNFOLLOW</button>
                   : <button onClick={() => {
-                    props.follow(u.id)
+                    this.props.follow(u.id)
                   }}>FOLLOW</button>}
-
               </div>
             </div>
             <div className={styles.message}>
@@ -71,14 +42,13 @@ const Users = (props) => {
                 <div>{"u.location.country"}</div>
                 <div>{"u.location.city"}</div>
               </div>
-
             </div>
           </div>
         </div>)
       }
-
-    </div>
-  )
-};
+      </div>
+    )
+  };
+}
 
 export default Users;
