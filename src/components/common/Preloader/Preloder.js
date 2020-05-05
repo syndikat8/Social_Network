0af53e9b4986c782fc89d62005f9ee1e19aa08a1
Dima Matusik <k.syndikat@gmail.com>
2020-05-05 13:38:36 +0300

    add Preloader

---
+++ src/components/common/Preloader/Preloder.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from './Preloader.module.css';
+
+
+
+const Preloader = () => {
+  return <div className={styles.loader}>
+
+  </div>
+
+
+}
+
+export default Preloader;
