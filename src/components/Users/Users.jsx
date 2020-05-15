95a00b8be73b9c304b55f68b27ad892c63c65196
Dima Matusik <k.syndikat@gmail.com>
2020-05-15 14:17:45 +0300

    add follow/ unfollow

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -2,60 +2,84 @@ import React from "react";
 import styles from './Users.module.css';
 import userPhoto from "../../assets/images/images.png"
 import {NavLink} from "react-router-dom";
-const Users = (props) =>  {
+import * as axios from "axios";
+
+const Users = (props) => {
   let pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
 
   let pages = []
   for (let i = 1; i <= pagesCount; i++) {
     pages.push(i)
   }
-    return (
+  return (
+    <div>
+
       <div>
+        {pages.map(p => {
+          return <span
+            className={props.currentPage === p && styles.selectedPage}
+            onClick={() => {
+              props.onPageChanged(p)
+            }}> {p}</span>
+        })}
+      </div>
 
-        <div>
-          {pages.map(p => {
-            return <span
-              className={props.currentPage === p && styles.selectedPage}
-              onClick={ () => {props.onPageChanged(p)} }> {p}</span>
-          })}
-        </div>
-
-        {props.users.map(u => <div key={u.id}>
-          <div className={styles.users}>
-            <div className={styles.avaButton}>
-              <div>
-                 <NavLink to={"/profile/" + u.id}>
+      {props.users.map(u => <div key={u.id}>
+        <div className={styles.users}>
+          <div className={styles.avaButton}>
+            <div>
+              <NavLink to={"/profile/" + u.id}>
                 <img
                   src={u.photos.small != null ? u.photos.small : userPhoto}
                   className={styles.userPhoto}/>
-                 </NavLink>
-              </div>
-              <div>
-                {u.followed
-                  ? <button onClick={() => {
-                    props.unfollow(u.id)
-                  }}>UNFOLLOW</button>
-                  : <button onClick={() => {
-                    props.follow(u.id)
-                  }}>FOLLOW</button>}
-              </div>
+              </NavLink>
             </div>
-            <div className={styles.message}>
-              <div>
+            <div>
+              {u.followed
+                ? <button onClick={() => {
+                  axios.delete(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {
+                    withCredentials: true,
+                    headers: {
+                      "API-KEY": "a4868654-1346-4601-9c9f-2bf29679e35a"
+                    }
+                  })
+                    .then(response => {
+                      if (response.data.resultCode === 0) {
+                        props.unfollow(u.id)
+                      }
+                    })
+                }}>UNFOLLOW</button>
+                  : <button onClick={() => {
+                    axios.post(`https://social-network.samuraijs.com/api/1.0/follow/${u.id}`, {}, {
+                      withCredentials: true,
+                      headers: {
+                        "API-KEY": "a4868654-1346-4601-9c9f-2bf29679e35a"
+                      }
+                    })
+                      .then(response => {
+                        if (response.data.resultCode === 0) {
+                          props.follow(u.id)
+                        }
+                      })
+                  }}>FOLLOW</button>}
+                </div>
+                </div>
+                <div className={styles.message}>
+                <div>
                 <div>{u.name}</div>
                 <div>{u.status}</div>
-              </div>
-              <div>
+                </div>
+                <div>
                 <div>{"u.location.country"}</div>
                 <div>{"u.location.city"}</div>
-              </div>
-            </div>
-          </div>
-        </div>)
-        }
-      </div>
-    )
-  };
+                </div>
+                </div>
+                </div>
+                </div>)
+                }
+                </div>
+                )
+                };
 
 
-export default Users;
+                export default Users;
