1dad0570e849fba135c2508c713f24f0b3e505c2
Dima Matusik <k.syndikat@gmail.com>
2020-06-15 16:38:11 +0300

    add users-selected

--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import {connect} from "react-redux";
 import Users from "./Users";
 import {
-  follow, getUsers,
+  follow, requestUsers,
   setCurrentPage,
   toggleFollowingProgress,
   unfollow
@@ -10,17 +10,25 @@ import {
   from "../../redux/users-reducer";
 import Preloader from "../common/Preloader/Preloder";
 import {compose} from "redux";
+import {
+  getCurrentPage,
+  getFollowingInProgress,
+  getIsFetching,
+  getPageSize,
+  getTotalUsersCount,
+  getUsers
+} from "../../redux/users-selectors";
 
 
 
 class UsersContainer extends React.Component {
 
   componentDidMount() {
-    this.props.getUsers(this.props.currentPage, this.props.pageSize)
+    this.props.requestUsers(this.props.currentPage, this.props.pageSize)
   }
 
   onPageChanged = (pageNumber) => {
-    this.props.getUsers(pageNumber, this.props.pageSize)
+    this.props.requestUsers(pageNumber, this.props.pageSize)
   }
 
   render() {
@@ -44,15 +52,15 @@ class UsersContainer extends React.Component {
 
 let mapStateToProps = (state) => {
   return {
-    users: state.usersPage.users,
-    pageSize: state.usersPage.pageSize,
-    totalUsersCount: state.usersPage.totalUsersCount,
-    currentPage: state.usersPage.currentPage,
-    isFetching: state.usersPage.isFetching,
-    followingInProgress: state.usersPage.followingInProgress,
+    users: getUsers(state),
+    pageSize: getPageSize(state),
+    totalUsersCount: getTotalUsersCount(state),
+    currentPage: getCurrentPage(state),
+    isFetching: getIsFetching(state),
+    followingInProgress: getFollowingInProgress(state),
   }
 }
 
 export default compose(
-  connect(mapStateToProps,{toggleFollowingProgress, follow, unfollow, setCurrentPage, getUsers})
+  connect(mapStateToProps,{toggleFollowingProgress, follow, unfollow, setCurrentPage, requestUsers})
 )(UsersContainer)
