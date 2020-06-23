3d054bf7f168418cab4f6bb8cd3c3f4a6b99699b
Dima Matusik <k.syndikat@gmail.com>
2020-06-23 13:28:03 +0300

    add React.memo

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,22 +3,22 @@ import styles from '../MyPosts/MyPosts.module.css';
 import Post from "./Post/Post";
 import PostsFormRedux from "./PostsForm/PostsForm";
 
-const MyPosts = (props) => {
-  let postsElement = props.posts.map(p => (<Post message={p.message} likesCount={p.likesCount}/>));
+const MyPosts = React.memo((props) => {
+    let postsElement = props.posts.map(p => (<Post message={p.message} likesCount={p.likesCount}/>));
 
-  let addNewPost = (value) => {
-    props.addPost(value.newPostText)
-  }
-  return (
-    <div className={styles.postsBlock}>
-      <h3>My posts</h3>
-      <PostsFormRedux onSubmit={addNewPost}/>
-      <div className={styles.posts}>
-        {postsElement}
+    let addNewPost = (value) => {
+      props.addPost(value.newPostText)
+    }
+    return (
+      <div className={styles.postsBlock}>
+        <h3>My posts</h3>
+        <PostsFormRedux onSubmit={addNewPost}/>
+        <div className={styles.posts}>
+          {postsElement}
+        </div>
       </div>
-    </div>
-  )
-};
-
+    )
+  }
+)
 
 export default MyPosts;
